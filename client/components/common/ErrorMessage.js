749d3dac83af1c68884358d2dce08b11da42f031
Daler <daler.developer@mail.ru>
2022-05-23 22:52:45 +0500

    changed color of error message and added feature of controlling it's alignment

--- client/components/common/ErrorMessage.js
+++ client/components/common/ErrorMessage.js
@@ -1,21 +1,29 @@
 import pt from 'prop-types'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import generateErrorMessage  from '../../utils/generateErrorMessage'
 
-const ErrorMessage = ({ type, ...rest }) => {
+const ErrorMessage = ({ type, align = 'left', ...rest }) => {
   return (
-    <StyledWrapper {...rest}>
+    <StyledWrapper {...rest} $align={align}>
       {generateErrorMessage(type)}
     </StyledWrapper>
   )
 }
 
 ErrorMessage.propTypes = {
- type: pt.string.isRequired
+  type: pt.string.isRequired,
+  align: pt.string
 }
 
 const StyledWrapper = styled.span`
-  color: red;
+  color: #ff4d4f;
+
+  ${({ $align }) => $align === 'start' && css`
+    text-align: left;
+  `}
+  ${({ $align }) => $align === 'center' && css`
+    text-align: center;
+  `}
 `
 
 export default ErrorMessage
